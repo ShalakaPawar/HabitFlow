3515e86e14edbfdf29b3409bf6d9876994b27824
ShalakaPawar <shalakapawar2301@gmail.com>
2026-03-09 02:02:52 +0530

    Finalize deployment fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { Lock, Zap } from 'lucide-react'
 
-export default function LoginPage() {
+function LoginPageContent() {
   const router = useRouter()
   const searchParams = useSearchParams()
   const [password, setPassword] = useState('')
@@ -74,5 +74,13 @@ export default function LoginPage() {
         </form>
       </div>
     </div>
+  )
+}
+
+export default function LoginPage() {
+  return (
+    <Suspense>
+      <LoginPageContent />
+    </Suspense>
   )
 }
